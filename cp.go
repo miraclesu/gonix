ac2319e73c7d011f6f2b93751562a43d888b10ef
polegone <polegone@users.noreply.github.com>
2015-05-08 17:56:17 -0400

    Create cp.go

---
+++ cp.go
@@ -0,0 +1,15 @@
+package main
+
+import "os"
+import "io"
+
+func main() {
+    //sf: source file, df: destination file
+    sf, _ := os.Open(os.Args[1])
+    sfinfo, _ := os.Stat(os.Args[1])
+    sfmode := sfinfo.Mode()
+    df, _ := os.OpenFile(os.Args[2], os.O_WRONLY | os.O_CREATE, sfmode)
+    io.Copy(df,sf)
+    sf.Close()
+    df.Close()
+}
