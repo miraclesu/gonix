67f385626865371c3302b1701919096c505018b0
polegone <polegone@users.noreply.github.com>
2015-05-08 15:24:43 -0400

    Create seq.go

---
+++ seq.go
@@ -0,0 +1,23 @@
+package main
+
+import "os"
+import "fmt"
+
+func main() {
+    if(len(os.Args)==2) {
+        var end int
+        fmt.Sscanf(os.Args[1],"%d",&end)
+        for i:=1; i<=end; i++ { fmt.Println(i) }
+    } else if(len(os.Args)==3) {
+        var start, end int
+        fmt.Sscanf(os.Args[1],"%d",&start)
+        fmt.Sscanf(os.Args[2],"%d",&end)
+        for i:=start; i<=end; i++ { fmt.Println(i) }
+    } else if(len(os.Args)==4) {
+        var start, inc, end int
+        fmt.Sscanf(os.Args[1],"%d",&start)
+        fmt.Sscanf(os.Args[2],"%d",&inc)
+        fmt.Sscanf(os.Args[3],"%d",&end)
+        for i:=start; i<=end; i+=inc { fmt.Println(i) }
+    }
+}
