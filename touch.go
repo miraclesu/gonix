bbb0c8fca0c9e712e010e227ffae39a78ddf92c4
polegone <polegone@users.noreply.github.com>
2015-05-09 13:38:10 -0400

    Updated touch. Now has -c flag and can work on multiple files.

--- touch.go
+++ touch.go
@@ -1,16 +1,27 @@
 package main
 
 import "os"
+import "flag"
 import "time"
+import "log"
 
 func main() {
-	filename := os.Args[1]
-	_, err := os.Stat(filename)
-	if err == nil {
-		now := time.Now()
-		os.Chtimes(filename, now, now)
-	} else {
-		f, _ := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
-		f.Close()
+	cFlag := flag.Bool("c",false,"do not create file")
+	flag.Parse()
+	if len(flag.Args())>0 {
+		for i:=0; i<len(flag.Args()); i++ {
+			filename := flag.Arg(i)
+			_, err := os.Stat(filename)
+			if err == nil {
+				now := time.Now()
+				os.Chtimes(filename, now, now)
+			} else {
+				if !(*cFlag) {
+					f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
+					f.Close()
+					if err != nil {log.Fatal(err)}
+				}
+			}
+		}
 	}
 }
