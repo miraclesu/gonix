900a23867d9e4ef1aaa20abb81221d8f9c4d108f
polegone <polegone@users.noreply.github.com>
2015-05-08 22:57:25 -0400

    Use error checking in cat
    It should now crash if you give it a nonexisting file.

--- cat.go
+++ cat.go
@@ -2,15 +2,18 @@ package main
 
 import "os"
 import "fmt"
+import "log"
 import "io/ioutil"
 
 func main() {
 	if len(os.Args) == 1 {
-		bytes, _ := ioutil.ReadAll(os.Stdin)
+		bytes, err := ioutil.ReadAll(os.Stdin)
+		if (err != nil) {log.Fatal(err)}
 		fmt.Printf("%s", string(bytes))
 	} else {
 		for i := 1; i < len(os.Args); i++ {
-			bytes, _ := ioutil.ReadFile(os.Args[i])
+			bytes, err := ioutil.ReadFile(os.Args[i])
+			if (err != nil) {log.Fatal(err)}
 			fmt.Printf("%s", string(bytes))
 		}
 	}
