f8874bcee71116b3c7697dbaba3e145481622ddc
polegone <polegone@users.noreply.github.com>
2015-05-09 17:52:53 -0400

    Update dirname.go

--- dirname.go
+++ dirname.go
@@ -9,7 +9,7 @@ func main() {
 		pathname := os.Args[1]
 		//If the given path ends in a slash, delete the last character of it (the slash)
 		if len(pathname)>0 && pathname[len(pathname)-1] == '/' {
-			pathname = pathname[:1]
+			pathname = pathname[:len(pathname)-1]
 		}
 		fmt.Println(path.Dir(pathname))
 	}
