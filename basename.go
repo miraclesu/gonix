40043f7871abc09f6410db987fbbf7f8422ee51d
polegone <polegone@users.noreply.github.com>
2015-05-09 12:51:15 -0400

    Update basename.go

--- basename.go
+++ basename.go
@@ -6,6 +6,7 @@ import "path"
 
 func main() {
 	if len(os.Args) > 1 {
+		pathname := os.Args[1]
 		//If the given path ends in a slash, delete the last character of it (the slash)
 		if pathname[len(pathname)-1] == '/' {
 			pathname = pathname[:1]
