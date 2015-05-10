d74b56c9147993e038cd698fece7c87d2f6aa785
polegone <polegone@users.noreply.github.com>
2015-05-09 22:59:32 -0400

    use switch case

--- seq.go
+++ seq.go
@@ -4,20 +4,21 @@ import "os"
 import "fmt"
 
 func main() {
-	if len(os.Args) == 2 {
+	switch len(os.Args {
+	case 2:
 		var end int
 		fmt.Sscanf(os.Args[1], "%d", &end)
 		for i := 1; i <= end; i++ {
 			fmt.Println(i)
 		}
-	} else if len(os.Args) == 3 {
+	case 3:
 		var start, end int
 		fmt.Sscanf(os.Args[1], "%d", &start)
 		fmt.Sscanf(os.Args[2], "%d", &end)
 		for i := start; i <= end; i++ {
 			fmt.Println(i)
 		}
-	} else if len(os.Args) == 4 {
+	case 4:
 		var start, inc, end int
 		fmt.Sscanf(os.Args[1], "%d", &start)
 		fmt.Sscanf(os.Args[2], "%d", &inc)
