cefacd2a7bdc0290253e1c6092c1491ba5cd165a
polegone <polegone@users.noreply.github.com>
2015-05-10 17:50:47 -0400

    add ability to create parent directories (-p flag)

--- mkdir.go
+++ mkdir.go
@@ -1,12 +1,19 @@
 package main
 
 import "os"
+import "flag"
 import "log"
 
 func main() {
-	if len(os.Args)>0 {
-		for i:=1; i<len(os.Args); i++ {
-			err := os.Mkdir(os.Args[i], 0644)
+	pFlag = flag.Bool("p",false,"create parent directories if needed")
+	flag.Parse()
+	if len(flag.Args())>0 {
+		for i:=0; i<len(flag.Args()); i++ {
+			if (*pFlag) {
+				err := os.MkdirAll(flag.Arg(i), 0644)
+			} else {
+				err := os.Mkdir(flag.Arg(i), 0644)
+			}
 			if err != nil {log.Fatal(err)}
 		}
 	}
