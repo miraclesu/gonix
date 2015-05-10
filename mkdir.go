5e61aa8e67dc24ef28393b6f6e8b8456aadc5c11
polegone <polegone@users.noreply.github.com>
2015-05-10 17:46:38 -0400

    add error checking to mkdir

--- mkdir.go
+++ mkdir.go
@@ -1,11 +1,13 @@
 package main
 
 import "os"
+import "log"
 
 func main() {
 	if len(os.Args)>0 {
 		for i:=1; i<len(os.Args); i++ {
-			os.Mkdir(os.Args[i], 0644)
+			err := os.Mkdir(os.Args[i], 0644)
+			if err != nil {log.Fatal(err)}
 		}
 	}
 }
