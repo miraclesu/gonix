baa0b2fe1fdd9e43f41a072d74741bf825387c88
polegone <polegone@users.noreply.github.com>
2015-05-12 17:42:57 -0400

    use pflag instead of flag

--- mkdir.go
+++ mkdir.go
@@ -1,11 +1,11 @@
 package main
 
 import "os"
-import "flag"
+import flag "github.com/ogier/pflag"
 import "log"
 
 func main() {
-	pFlag := flag.Bool("p",false,"create parent directories if needed")
+	pFlag := flag.BoolP("parents", "p", false, "create parent directories if needed")
 	flag.Parse()
 	if len(flag.Args())>0 {
 		for i:=0; i<len(flag.Args()); i++ {
